2dbdd045f87d8daffdae3468b1f95c48ebe8251b
jgyun <jgyun@kr.ibm.com>
2018-09-05 06:09:15 +0900

    add java example

--- pbe.js
+++ pbe.js
@@ -9,9 +9,10 @@ const crypto = require('crypto');
 * KDF
 */
 const KDF= (password,salt,iterations) =>{
-    var pwd = new Buffer(password,'utf-8');
-    var key = Buffer.concat([pwd, salt]);
-    var i;
+    const pwd = new Buffer(password,'utf-8');
+    let key = Buffer.concat([pwd, salt]);
+    let i;
+
     for (i = 0; i < iterations; i+=1) {
       key = crypto.createHash("md5").update(key).digest();
     }
@@ -22,35 +23,53 @@ const KDF= (password,salt,iterations) =>{
 * getKeyIV
 */
 const getKeyIV= (password,salt,iterations) =>{
-    var key = KDF(password,salt,iterations);
-    var keybuf = new Buffer(key,'binary').slice(0,8);
-    var ivbuf = new Buffer(key,'binary').slice(8,16);
+    const key = KDF(password,salt,iterations);
+    const keybuf = new Buffer(key,'binary').slice(0,8);
+    const ivbuf = new Buffer(key,'binary').slice(8,16);
+
     return [ keybuf, ivbuf ];
 }
 
 /*
 * encrypt
 */
-const encrypt=(payload,password,salt,iterations,cb)=>{
-    var kiv = getKeyIV(password,salt,iterations);
-    var cipher = crypto.createCipheriv('des', kiv[0],kiv[1]);
-    var encrypted = [];
-    encrypted.push(cipher.update(payload,'utf-8','hex'));
-    encrypted.push(cipher.final('hex'));
-    return cb(undefined,new Buffer(encrypted.join(''),'hex').toString('base64'));
+const encrypt=(payload,password,salt,iterations)=>{
+
+    const kiv = getKeyIV(password,salt,iterations);
+    const cipher = crypto.createCipheriv('des', kiv[0],kiv[1]);
+    const encrypted = [];
+  
+    return new Promise((resolve, reject) => {
+        try {
+            encrypted.push(cipher.update(payload,'utf-8','hex'));
+            encrypted.push(cipher.final('hex'));
+
+            return resolve(new Buffer(encrypted.join(''),'hex').toString('base64'));
+        } catch (err) {
+            reject(err)
+        }
+    })
 }
 
 /*
 * decrypt
 */
-const decrypt=(payload,password,salt,iterations,cb) =>{
-    var encryptedBuffer = new Buffer(payload,'base64');
-    var kiv = getKeyIV(password,salt,iterations);
-    var decipher = crypto.createDecipheriv('des', kiv[0],kiv[1]);
-    var decrypted = [];
-    decrypted.push(decipher.update(encryptedBuffer));
-    decrypted.push(decipher.final());
-    return cb(undefined, decrypted.join(''));
+const decrypt=(payload,password,salt,iterations) =>{
+    const encryptedBuffer = new Buffer(payload,'base64');
+    const kiv = getKeyIV(password,salt,iterations);
+    const decipher = crypto.createDecipheriv('des', kiv[0],kiv[1]);
+    const decrypted = [];
+
+    return new Promise((resolve, reject) => {
+        try {
+            decrypted.push(decipher.update(encryptedBuffer));
+            decrypted.push(decipher.final());
+    
+            return resolve(decrypted.join(''));
+        } catch (err) {
+            reject(err)
+        }
+    })
 }
 
 module.exports = {
--- pbedemo.js
+++ pbedemo.js
@@ -1,14 +1,44 @@
   
-  var pbewithmd5anddes = require('./pbe')
-  var password = 'nexweb';
-  var iterations = 19;
+  /*
+  public String Encrypted(String plainText) {
+    //Key generation for enc and desc
+    KeySpec keySpec = new PBEKeySpec(key.toCharArray(), salt, iterationCount);
+    SecretKey scr_key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
+    // Prepare the parameter to the ciphers
+    AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
+
+    //Enc process
+    ecipher = Cipher.getInstance(scr_key.getAlgorithm());
+    ecipher.init(Cipher.ENCRYPT_MODE, scr_key, paramSpec);
+    String charSet = "UTF-8";
+    byte[] in = plainText.getBytes(charSet);
+    byte[] out = ecipher.doFinal(in);
+    String encStr = new sun.misc.BASE64Encoder().encode(out);
+
+    return encStr
+  }
+  */
+  
+  const pbewithmd5anddes = require('./pbe')
+  const password = 'nexweb';
+  const iterations = 19;
   //var salt = new Buffer('32121F055C39F5A75','hex');
-  var salt = new Buffer('d99bce325735e303','hex');
-  console.log('salt:', salt);
-  pbewithmd5anddes.encrypt('test',password,salt,iterations,function(err,msg) {
+  //var salt = new Buffer('d99bce325735e303','hex');
+  //var salt = new Buffer('d99bce325735e303','hex');
+  const salt = Buffer.from([0xD9, 0x9B,0xce,0x32,
+  0x57, 0x35, 0xE3,0x03]);
+
+  let plainText = process.argv[2];
+  
+  if (!plainText) {
+    plainText = 'test';
+  }
+  pbewithmd5anddes.encrypt(plainText, password, salt, iterations)
+  .then(msg =>{
     console.log('encrypted: ' + msg);
     // eat your own dogfood
-    pbewithmd5anddes.decrypt(msg,password,salt,iterations,function(err,msg) {
+    pbewithmd5anddes.decrypt(msg, password, salt, iterations)
+    .then(msg=>{
       console.log('decrypted: ' + msg);
     });
   });
