4b1ac77d11323f5888fdeeff447ca3b402f3a03e
jgyun <jgyun@kr.ibm.com>
2018-09-05 05:39:22 +0900

    refactoring es5 to es6

--- pbe.js
+++ pbe.js
@@ -3,11 +3,11 @@
 /*
  * Emulates Java's PBEWITHMD5ANDDES for node.js
  */
-var crypto = require('crypto');
+const crypto = require('crypto');
 
-var pbewithmd5anddes = {
+//let pbewithmd5anddes = {
 
-  KDF: function(password,salt,iterations) {
+const KDF= (password,salt,iterations) =>{
     var pwd = new Buffer(password,'utf-8');
     var key = Buffer.concat([pwd, salt]);
     var i;
@@ -15,33 +15,38 @@ var pbewithmd5anddes = {
       key = crypto.createHash("md5").update(key).digest();
     }
     return key;
-  },
+}
 
-  getKeyIV: function(password,salt,iterations) {
-    var key = this.KDF(password,salt,iterations);
+const getKeyIV= (password,salt,iterations) =>{
+    var key = KDF(password,salt,iterations);
     var keybuf = new Buffer(key,'binary').slice(0,8);
     var ivbuf = new Buffer(key,'binary').slice(8,16);
     return [ keybuf, ivbuf ];
-  },
+}
 
-  encrypt: function(payload,password,salt,iterations,cb) {
-    var kiv = this.getKeyIV(password,salt,iterations);
+const encrypt=(payload,password,salt,iterations,cb)=>{
+    var kiv = getKeyIV(password,salt,iterations);
     var cipher = crypto.createCipheriv('des', kiv[0],kiv[1]);
     var encrypted = [];
     encrypted.push(cipher.update(payload,'utf-8','hex'));
     encrypted.push(cipher.final('hex'));
     return cb(undefined,new Buffer(encrypted.join(''),'hex').toString('base64'));
-  },
+}
 
-  decrypt: function(payload,password,salt,iterations,cb) {
+const decrypt=(payload,password,salt,iterations,cb) =>{
     var encryptedBuffer = new Buffer(payload,'base64');
-    var kiv = this.getKeyIV(password,salt,iterations);
+    var kiv = getKeyIV(password,salt,iterations);
     var decipher = crypto.createDecipheriv('des', kiv[0],kiv[1]);
     var decrypted = [];
     decrypted.push(decipher.update(encryptedBuffer));
     decrypted.push(decipher.final());
     return cb(undefined, decrypted.join(''));
-  }
-};
+}
 
-module.exports = pbewithmd5anddes;
+
+module.exports = {
+    KDF: KDF,
+    getKeyIV: getKeyIV,
+    encrypt: encrypt,
+    decrypt: decrypt,
+};
